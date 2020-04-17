ca5830c37cf443bc98c29295f86b485cd2969ff1
cielavenir <cielartisan@gmail.com>
2020-04-17 09:35:53 +0900

    temporary add mujin pytest

---
+++ mujin_debian_stretch_pytest2.sh
@@ -0,0 +1,3 @@
+#docker build -t index.docker.io/mujin/debian:stretch_pytest2 mujin_debian_stretch_pytest2
+docker build -t index.docker.io/cielavenir/pytest:mujin_stretch2 mujin_debian_stretch_pytest2
+
