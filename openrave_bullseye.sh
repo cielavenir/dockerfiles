bf6171ea329a9acb0e9153dbf6799960af5cfc78
cielavenir <cielartisan@gmail.com>
2023-03-30 15:33:56 +0900

    add openrave Dockerfile

---
+++ openrave_bullseye.sh
@@ -0,0 +1,1 @@
+docker build -t index.docker.io/cielavenir/openrave:bullseye --build-arg BASE=debian:bullseye-slim openrave
---
+++ openrave_jammy.sh
@@ -0,0 +1,1 @@
+docker build -t index.docker.io/cielavenir/openrave:jammy --build-arg BASE=ubuntu:jammy openrave
